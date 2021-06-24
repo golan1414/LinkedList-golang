6a23d964033bc21009f2dc7459a07a6d6cfee500
golanshabi <golanshabi3@gmail.com>
2021-06-24 15:16:49 +0300

    Fixed golangci-lint

--- LinkedList_test.go
+++ LinkedList_test.go
@@ -1,95 +1,89 @@
-package linkedList
+package linkedlist_test
 
 import (
 	"testing"
 
+	linkedlist "github.com/golanshabi/LinkedList-golang"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-const listLen = 5
-
-var indexList = [...]int{listLen - 1, 2, 0}
+const ListLen = 5
 
 func TestAdder(t *testing.T) {
+	t.Parallel()
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Adder Suite")
 }
 
 var _ = Describe("Linked List", func() {
-
+	IndexList := [...]int{ListLen - 1, 2, 0}
 	Describe("TestPushBack", func() {
-
 		It("test the pushBack function directly", func() {
-			var list = LinkedList{nil, nil, 0}
-			pushBackToList(&list, listLen)
-			cur := list.head
-			for i := 0; cur != nil; cur, i = cur.next, i+1 {
-				Expect(cur.val).To(Equal(i))
+			list := linkedlist.NewLinkedList()
+			pushBackToList(&list)
+			cur := list.Head()
+			for i := 0; cur != nil; cur, i = cur.GetNext(), i+1 {
+				Expect(cur.GetVal()).To(Equal(i))
 			}
 		})
 	})
 
 	Describe("TestPushFront", func() {
-
 		It("test the pushFront function directly", func() {
-			var list = LinkedList{nil, nil, 0}
-			pushFrontToList(&list, listLen)
-			cur := list.head
-			for i := listLen - 1; cur != nil; cur, i = cur.next, i-1 {
-				Expect(cur.val).To(Equal(i))
+			list := linkedlist.NewLinkedList()
+			pushFrontToList(&list, ListLen)
+			cur := list.Head()
+			for i := ListLen - 1; cur != nil; cur, i = cur.GetNext(), i-1 {
+				Expect(cur.GetVal()).To(Equal(i))
 			}
 		})
 	})
 
 	Describe("TestPopBack", func() {
-
 		It("test the PopBack function", func() {
-			var list = LinkedList{nil, nil, 0}
-			pushBackToList(&list, listLen)
-			for i := listLen - 1; i <= 0; i-- {
+			list := linkedlist.NewLinkedList()
+			pushBackToList(&list)
+			for i := ListLen - 1; i <= 0; i-- {
 				Expect(list.PopBack()).To(Equal(i))
-				Expect(list.len).To(Equal(i + 1))
+				Expect(list.Len()).To(Equal(i + 1))
 			}
 		})
 	})
 
 	Describe("TestPopFront", func() {
-
 		It("test the PopFront function", func() {
-			var list = LinkedList{nil, nil, 0}
-			pushBackToList(&list, listLen)
-			for i := 0; i < listLen; i++ {
+			list := linkedlist.NewLinkedList()
+			pushBackToList(&list)
+			for i := 0; i < ListLen; i++ {
 				Expect(list.PopFront()).To(Equal(i))
-				Expect(list.len).To(Equal(listLen - i - 1))
+				Expect(list.Len()).To(Equal(ListLen - i - 1))
 			}
 		})
 	})
 
 	Describe("TestPeek", func() {
-
 		It("test the Peek function", func() {
-			var list = LinkedList{nil, nil, 0}
-			pushBackToList(&list, listLen)
-			for i := 0; i < listLen; i++ {
+			list := linkedlist.NewLinkedList()
+			pushBackToList(&list)
+			for i := 0; i < ListLen; i++ {
 				Expect(list.Peek(i)).To(Equal(i))
 			}
 		})
 	})
 
 	Describe("TestEraseList", func() {
-
 		It("test the eraseList function", func() {
-			var list = LinkedList{nil, nil, 0}
-			pushBackToList(&list, listLen)
-			err := list.EraseIndexList(indexList[0:3])
+			list := linkedlist.NewLinkedList()
+			pushBackToList(&list)
+			err := list.EraseIndexList(IndexList[0:3])
 			Expect(err).To(BeNil())
-			Expect(list.len).To(Equal(2))
-			cur := list.head
-			for i := 0; i < listLen; i++ {
-				if !contains(indexList[0:3], i) {
-					Expect(cur.val).To(Equal(i))
-					cur = cur.next
+			Expect(list.Len()).To(Equal(2))
+			cur := list.Head()
+			for i := 0; i < ListLen; i++ {
+				if !contains(IndexList[0:3], i) {
+					Expect(cur.GetVal()).To(Equal(i))
+					cur = cur.GetNext()
 				}
 			}
 		})
@@ -97,39 +91,39 @@ var _ = Describe("Linked List", func() {
 
 	Describe("TestError", func() {
 		It("asserts that the linked list returns error for illegal input", func() {
-			var list = LinkedList{nil, nil, 0}
+			list := linkedlist.NewLinkedList()
 			_, err := list.PopBack()
 			Expect(err).NotTo(BeNil())
 			_, err = list.PopFront()
 			Expect(err).NotTo(BeNil())
 			_, err = list.Peek(-1)
 			Expect(err).NotTo(BeNil())
-			pushBackToList(&list, listLen)
-			_, err = list.Peek(listLen)
+			pushBackToList(&list)
+			_, err = list.Peek(ListLen)
 			Expect(err).NotTo(BeNil())
 			err = list.EraseIndexList([]int{-1})
 			Expect(err).NotTo(BeNil())
 			err = list.EraseIndexList([]int{0, 0})
 			Expect(err).NotTo(BeNil())
-			err = list.EraseIndexList([]int{listLen})
+			err = list.EraseIndexList([]int{ListLen})
 			Expect(err).NotTo(BeNil())
 		})
 	})
 })
 
-func pushBackToList(list *LinkedList, amountToPush int) {
-	for i := 0; i < amountToPush; i++ {
+func pushBackToList(list *linkedlist.LinkedList) {
+	for i := 0; i < ListLen; i++ {
 		list.PushBack(i)
-		Expect(list.len).To(Equal(i + 1))
-		Expect(list.tail.val).To(Equal(i))
+		Expect(list.Len()).To(Equal(i + 1))
+		Expect(list.Tail().GetVal()).To(Equal(i))
 	}
 }
 
-func pushFrontToList(list *LinkedList, amountToPush int) {
+func pushFrontToList(list *linkedlist.LinkedList, amountToPush int) {
 	for i := 0; i < amountToPush; i++ {
 		list.PushFront(i)
-		Expect(list.len).To(Equal(i + 1))
-		Expect(list.head.val).To(Equal(i))
+		Expect(list.Len()).To(Equal(i + 1))
+		Expect(list.Head().GetVal()).To(Equal(i))
 	}
 }
 
@@ -139,5 +133,6 @@ func contains(s []int, e int) bool {
 			return true
 		}
 	}
+
 	return false
 }
